8f694f4a056c5734ebd400894a0c8b8b4c727a06
Anton <sozidatel79>
2017-05-09 22:40:27 +0300

    ES6 Components refactoring

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,18 +1,15 @@
 var React = require('react');
-var WeatherMessage = React.createClass({
-    render: function(){
-        var {location, temp, cod} = this.props;
-        console.log(cod);
-        var message;
-        if(cod == '404'){
-            message = location;
-        }else{
-            message = `It's it ${temp} ℃  in  ${location}`;
-        }
-        return (
-            <h3 className="message">{message}</h3>
-        );
-    }
-});
 
+var WeatherMessage = (props) => {
+    var {location, temp, cod} = props;
+    var message;
+    if(cod == '404'){
+        message = location;
+    }else{
+        message = `It's it ${temp} ℃  in  ${location}`;
+    }
+    return (
+        <h3 className="message">{message}</h3>
+    );
+}
 module.exports = WeatherMessage;
