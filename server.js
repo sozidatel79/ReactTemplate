69ae3d1bd8a6941f70f8ea59a5cc7562cdd72e96
Anton <sozidatel79>
2017-05-10 01:28:16 +0300

    package.json updated with start command

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.port || 3000;
-app.use(function (req, res, next) {
-   if (req.header['x-forwarded-proto'] == 'http'){
-        next();
-   } else{
-        res.redirect('http://' + req.hostname + req.url);
-   }
-});
 app.use(express.static('public'));
 app.listen(PORT, function () {
    console.log('Listen on port ' + PORT);
