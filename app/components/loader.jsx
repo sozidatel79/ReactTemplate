6228c1fedabcf09531d31969d24fc4663c66d28b
Anton <sozidatel79>
2017-05-10 22:52:49 +0300

    component loader added2

---
+++ app/components/loader.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var loader = React.createClass({
+	render: function() {
+		return (
+			<div className="text-center">
+				<image src="../../public/img/spin.gif" />	
+			</div>
+		);
+	}
+});
+
+module.exports = loader;
