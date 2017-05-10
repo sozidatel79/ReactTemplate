8e217402dac9c237884abe0111501e1c502fb6ae
Anton <sozidatel79>
2017-05-11 00:27:18 +0300

    modal message changed

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -22,8 +22,8 @@ var ErrorModal = React.createClass({
        var {errorMessage, cod} = this.props;
        return (
            <div className="reveal large text-center" id="error-modal" data-reveal="">
-               <h3>Http: {cod}</h3>
-               <p className='lead'>Error: {errorMessage}</p>
+               <h3>Error: {cod}</h3>
+               <p className='lead'>{errorMessage}</p>
                <div>
                  <button onClick={this.closeErrorModal} className="button hollow small" data-close="" type="button">
                      <span aria-hidden="true">Close</span>
